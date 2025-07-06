a70d628cd2671e1a764ff70fc30ec44ae5f13efd
kavin-jiwane <kavinjiwane8113@gmail.com>
2025-07-07 00:53:48 +0530

    About Section added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -136,13 +136,13 @@ const Hero = () => {
 
                 <h1 className='special-font hero-heading absolute bottom-5
                 right-5 z-40 text-blue-75'>
-                    Gaming
+                    G<b>a</b>ming
                 </h1>
 
                 <div className='absolute left-0 top-0 z-40 size-full'>
                     <div className='mt-24 px-5 sm:px-10'>
                         <h1 className='special-font hero-heading text-blue-100'>
-                            Redefine
+                            Redefi<b>n</b>e
                         </h1>
 
                         <p className='mb-5 max-w-64 font-robert-regular
